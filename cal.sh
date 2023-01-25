89a674fdaeb5e3a511985178cd7f8e9696dde84a
prarthanaus0597 <100505947+prarthanaus0597@users.noreply.github.com>
2023-01-25 16:09:02 +0530

    Add files via upload

---
+++ cal.sh
@@ -0,0 +1,23 @@
+a=$1
+b=$2
+
+echo "Enter Choice: "
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+
+ch=$3
+
+case $ch in
+	1) res=`expr $a + $b `
+	;;
+	2) res=`expr $a - $b `
+	;;
+	3) res=`expr $a \* $b `
+	;;
+	4) res=`expr $a / $b `
+	;;
+	
+esac
+echo "Result: $res"
